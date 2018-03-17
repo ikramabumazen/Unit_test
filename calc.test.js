431ffa3d858eecb8bb6b412136a91babe7b401c2
Your Name <you@example.com>
2018-03-17 22:54:51 +0200

    clac.test.js _ test 2 integers one digit non-negative numbers

--- calc.test.js
+++ calc.test.js
@@ -1,5 +1,5 @@
-const sum = require('./calc');
+const addition = require('./calc');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+test('adds "1+2" to equal 3', () => {
+ expect(addition("1+2")).toBe(3);
+}); 
