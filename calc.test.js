e9d427689c530778223e667797623125681e0f6d
Your Name <you@example.com>
2018-03-17 23:35:42 +0200

    clac.test.js _ addition( ) _ test case 3 : double numbers

--- calc.test.js
+++ calc.test.js
@@ -7,4 +7,8 @@ const addition = require('./calc');
    test('adds "100+2" to equal 102', () => {
     expect(addition("100+2")).toBe(102);
    }); 
+   //double
+   test('adds "1.43+2.5" to equal 3', () => {
+    expect(addition("1.43+2.5")).toBe( 3.9299999999999997);
+   }); 
    
