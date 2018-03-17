d774447f4a983c28bdbde1bd849cd735cd9e51b2
Your Name <you@example.com>
2018-03-17 13:19:55 +0200

    calc.js _ problem : readFromConsole( ) returns undefined

--- calc.js
+++ calc.js
@@ -2,10 +2,15 @@
 function readFromConsole(){
 var stdin = process.openStdin();
 stdin.addListener("data", function(d) {
-    // note:  d is an object, and when converted to a string it will
-    // end with a linefeed.  so we (rather crudely) account for that  
-    // with toString() and then trim() 
-    console.log("you entered: [" + 
-        d.toString().trim() + "]");
-  });
+  //console.log(d.toString());
+  let userEntered= (d.toString().trim() );
+   //console.log("you Entered :[" +userEntered+ "]");
+
+return d.toString() ;
+       
+  });  
 }//readFromConsole()
+
+let readString =readFromConsole();
+
+console.log( readString);
