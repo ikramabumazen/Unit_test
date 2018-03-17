45117251c28a22cc151a640587c384388a3cb6fe
Your Name <you@example.com>
2018-03-17 20:46:14 +0200

    calc.js _ refactoring addition with isAddition( ) method

--- calc.js
+++ calc.js
@@ -2,21 +2,13 @@
 var stdin = process.openStdin();
 
 function readFromConsoleAndSplit(){
-
 stdin.addListener("data", function(d) {
   let str = d.toString().trim();
-// split 
 var res = str.split("");
-  console.log(res);
 
  // filter res array according operation
-//------------Addition--------------------------------
-                let sum = res.filter(item =>
-                item == "+");
-                if (sum.length !=0) {
-                  console.log("sum()");
-                } 
-//-----------Subtraction ------------------------------
+isAddition(res, str);       
+//--Subtraction--
 let sub = res.filter(item =>
   item == "-");
   if (sub.length !=0) {
@@ -44,6 +36,17 @@ let mod = res.filter(item =>
 
   });              
 }//readFromConsole()
+//--------------- Addition functions ---------------------------------
+//Isaddition 
+function isAddition(res,str){
+  let sum = res.filter(item =>item == "+");
+    if (sum.length !=0) {
+      addition(str);
+    }
+    else return ;
+}
 
-readFromConsoleAndSplit();
+//---------------------------------------------------------------------------
+
+readFromConsoleAndSplit(); 
 
